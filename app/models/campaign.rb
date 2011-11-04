ecc4e1f22b155dc72e59118aea4b2dac080c547c
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-04 13:09:18 -0400

    Calculate total contact attempts and revenue

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,9 @@
 class Campaign < ActiveRecord::Base
+  has_many :attempts
+  
+  def total_revenue
+    attempts.collect(&:value).compact.sum
+  end
   def avg_calls_per_day
     days = attempts.group_by(&:year_day)
     attempts.count/days.count.to_f
