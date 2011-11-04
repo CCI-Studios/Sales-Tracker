b4bac24d9379d8a88af31e057adbc0b55a518fe1
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-04 13:10:04 -0400

    View revenue/contact

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,6 +4,11 @@ class Campaign < ActiveRecord::Base
   def total_revenue
     attempts.collect(&:value).compact.sum
   end
+
+  def revenue_per_contact
+    total_revenue/attempts.count
+  end
+  
   def avg_calls_per_day
     days = attempts.group_by(&:year_day)
     attempts.count/days.count.to_f
