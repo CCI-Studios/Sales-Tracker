a4098401d39d31bf5dca417a94d91af7888e67c2
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-06 01:57:56 -0500

    Added admin user level

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111104203012) do
+ActiveRecord::Schema.define(:version => 20111106063158) do
 
   create_table "attempts", :force => true do |t|
     t.datetime "time"
@@ -75,6 +74,7 @@ ActiveRecord::Schema.define(:version => 20111104203012) do
     t.string   "persistence_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",             :default => false
   end
 
 end
