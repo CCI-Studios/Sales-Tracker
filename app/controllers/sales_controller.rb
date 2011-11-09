e6f7bbb0705a0b4a1ce13b4052b0f203cd719963
Jonathan Bennett <jbennett@ccistudios.com>
2011-11-08 22:52:39 -0500

    Removed sales XML

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,41 +3,23 @@ class SalesController < ApplicationController
   autocomplete :company, :title, :full => true
   
   # GET /sales
-  # GET /sales.xml
   def index
     @sales = Sale.page(params[:page])
     @title = "Sales"
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @sales }
-    end
   end
 
   # GET /sales/1
-  # GET /sales/1.xml
   def show
     @sale = Sale.find(params[:id])
     @title = "Sale ##{@sale.id}"
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @sale }
-    end
   end
 
   # GET /sales/new
-  # GET /sales/new.xml
   def new
     @sale = Sale.new
     @sale.campaign_id = 1
     @sale.user = current_user
     @title = "New Sale"
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @sale }
-    end
   end
 
   # GET /sales/1/edit
@@ -47,46 +29,30 @@ class SalesController < ApplicationController
   end
 
   # POST /sales
-  # POST /sales.xml
   def create
     @sale = Sale.new(params[:sale])
 
-    respond_to do |format|
-      if @sale.save
-        format.html { redirect_to(@sale, :notice => 'Sale was successfully created.') }
-        format.xml  { render :xml => @sale, :status => :created, :location => @sale }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @sale.errors, :status => :unprocessable_entity }
-      end
+    if @sale.save
+      redirect_to(@sale, :notice => 'Sale was successfully created.')
+    else
+      render :action => "new"
     end
   end
 
   # PUT /sales/1
-  # PUT /sales/1.xml
   def update
     @sale = Sale.find(params[:id])
 
-    respond_to do |format|
-      if @sale.update_attributes(params[:sale])
-        format.html { redirect_to(@sale, :notice => 'Sale was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @sale.errors, :status => :unprocessable_entity }
-      end
+    if @sale.update_attributes(params[:sale])
+      redirect_to(@sale, :notice => 'Sale was successfully updated.')
+    else
+      render :action => "edit"
     end
   end
 
   # DELETE /sales/1
-  # DELETE /sales/1.xml
   def destroy
     @sale = Sale.find(params[:id])
     @sale.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(sales_url) }
-      format.xml  { head :ok }
-    end
   end
 end
