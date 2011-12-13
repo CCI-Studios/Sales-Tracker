ef9b5aaff10092f64d74f91e5d8711f18377aa58
Jonathan Bennett <jbennett@ccistudios.com>
2011-12-13 17:04:34 -0500

    Fixed sale_items not having a product
    trying to add a "none" product caused sale_item with no product to be added. Also added a validation to ensure that it is specified
    
    Fixes #36

--- app/controllers/sale_items_controller.rb
+++ app/controllers/sale_items_controller.rb
@@ -5,11 +5,15 @@ class SaleItemsController < ApplicationController
   def create
     @sale = Sale.find(params[:sale_id])
     @item = @sale.sale_items.build(params[:sale_item])
-    @item.save
     
     respond_to do |format|
-      format.html { redirect_to edit_sale_path @sale }
-      format.js
+      if @item.save
+        format.html { redirect_to edit_sale_path @sale }
+        format.js
+      else
+        format.html { render :action => "new" }
+        format.js
+      end
     end
   end
 end
--- app/models/sale_item.rb
+++ app/models/sale_item.rb
@@ -2,6 +2,8 @@ class SaleItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :sale
   
+  validates :product_id, :presence => true
+  
   scope :filter_products, lambda { |search|
     joins(:product).
     where('products.title LIKE ?', "%#{search}%")
